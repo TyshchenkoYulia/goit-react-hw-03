0c33e0a373cc89a9cd6b059a49f3a7978a33c739
TyshchenkoYulia <145122847+TyshchenkoYulia@users.noreply.github.com>
2024-04-12 20:19:48 +0300

    added task 3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { nanoid } from "nanoid";
+import * as Yup from "yup";
 import contactCard from "../../contacts.json";
 import ContactForm from "../ContactForm/ContactForm";
 import SearchBox from "../SearchBox/SearchBox";
@@ -13,6 +15,29 @@ export default function App() {
     contact.name.toLowerCase().includes(search.toLowerCase())
   );
 
+  const validation = Yup.object().shape({
+    name: Yup.string()
+      .min(3, "Too Short!")
+      .max(50, "Too Long!")
+      .required("Required"),
+    number: Yup.string()
+      .min(3, "Too Short!")
+      .max(7, "Too Long!")
+      .required("Required"),
+  });
+
+  const initialValues = {
+    id: nanoid(),
+    name: "",
+    telNumber: "",
+  };
+
+  const addUser = (newUser) => {
+    setContacts((previewContact) => {
+      return [...previewContact, newUser];
+    });
+  };
+
   const deleteContact = (contactId) => {
     setContacts((previewContact) => {
       return previewContact.filter((contact) => contact.id !== contactId);
@@ -22,7 +47,11 @@ export default function App() {
   return (
     <div className={css.container}>
       <h1 className={css.title}>Phonebook</h1>
-      <ContactForm />
+      <ContactForm
+        value={initialValues}
+        onAddUser={addUser}
+        validation={validation}
+      />
       <SearchBox value={search} onFilter={setSearch} />
       <ContactList contacts={visibleContacts} onDelete={deleteContact} />
     </div>
